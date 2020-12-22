c7f9ee611e2ab4145f8737d5137cccd524d6895f
rgoshen <rick.goshen@gmail.com>
2020-12-22 11:07:33 -0700

    added automated submit application

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import time
 
 
 ACCOUNT_EMAIL = "rick.goshen@gmail.com"
-ACCOUNT_PASSWORD = "password"
+ACCOUNT_PASSWORD = "NitL10n$#1"
+PHONE = "520-639-0031"
 
 chrome_driver_path = "/home/rgoshen/Development/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
-driver.get("https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491&keywords=python%20developer&location=London%2C%20England%2C%20United%20Kingdom&redirect=false&position=1&pageNum=0")
+driver.get("https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491&keywords=marketing%20intern&location=London%2C%20England%2C%20United%20Kingdom&redirect=false&position=1&pageNum=0")
 
 
 sign_in_button = driver.find_element_by_link_text("Sign in")
@@ -22,3 +23,18 @@ username_input.send_keys(ACCOUNT_EMAIL)
 password_input = driver.find_element_by_id("password")
 password_input.send_keys(ACCOUNT_PASSWORD)
 password_input.send_keys(Keys.ENTER)
+
+# Locate the apply button
+time.sleep(2)
+apply_button = driver.find_element_by_css_selector(".jobs-s-apply button")
+apply_button.click()
+
+# If application requires phone number and the field is empty, then fill in the number.
+time.sleep(2)
+phone = driver.find_element_by_class_name("fb-single-line-text__input")
+if phone.text == "":
+    phone.send_keys(PHONE)
+
+# Submit the application
+submit_button = driver.find_element_by_css_selector("footer button")
+submit_button.click()
